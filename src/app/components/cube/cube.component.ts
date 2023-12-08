fc82d23776c7e31b15f17b3bec29755d24cab3c5
peggy-chien <peggychou0610@gmail.com>
2023-12-08 22:09:05 +0800

    feat(cube): add cube renderer
    Add cube renderer and provide outline for the cube.

--- src/app/components/cube/cube.component.ts
+++ src/app/components/cube/cube.component.ts
@@ -18,19 +18,19 @@ export class CubeComponent implements AfterViewInit {
    * cube properties
    */
   // rotation speed of cube on x-asis
-  @Input() public rotationSpeedX: number = 0.05;
+  @Input() public rotationSpeedX: number = 0.02;
   // rotaiton speed of cube on y-asis
   @Input() public rotationSpeedY: number = 0.01;
   // size of the cube
   @Input() public size: number = 200;
   // cube texture
-  @Input() public texture: string = './../../assets/texture.jpg';
+  @Input() public texture: string = '/assets/texture.jpg';
 
   /**
    * stage properties
    */
   // camera position on the z-asis
-  @Input() public cameraZ: number = 400;
+  @Input() public cameraZ: number = 300;
   // field of view of the camera
   @Input() public fieldOfView: number = 1;
   // Near and far clipping planes are imaginary planes located at two particular distances from the camera along the camera’s sight line.
@@ -52,8 +52,10 @@ export class CubeComponent implements AfterViewInit {
   // there are different options for materials, the main difference between most of them is how they react to light
   // e.g. MeshBasicMaterial (don't react to light), MeshLambertMaterial, MeshPhongMaterial
   private material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture) });
+  private outlineMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
 
   private cube: THREE.Mesh = new THREE.Mesh(this.geometory, this.material);
+  private outline: THREE.LineSegments = new THREE.LineSegments(new THREE.EdgesGeometry(this.geometory), this.outlineMaterial);
 
   private renderer!: THREE.WebGLRenderer;
 
@@ -75,6 +77,7 @@ export class CubeComponent implements AfterViewInit {
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0x000000);
     this.scene.add(this.cube);
+    this.scene.add(this.outline);
 
     // camera
     const aspectRatio = this.getAspectRatio();
@@ -98,11 +101,28 @@ export class CubeComponent implements AfterViewInit {
    * @memberof CubeComponent
    */
   private animateCube(): void {
-    this.cube.rotation.x = this.rotationSpeedX;
-    this.cube.rotation.y = this.rotationSpeedY;
+    this.outline.rotation.x = this.cube.rotation.x += this.rotationSpeedX;
+    this.outline.rotation.y = this.cube.rotation.y += this.rotationSpeedY;
   }
 
+  /**
+   * Start the rendering loop
+   * 
+   * @private
+   * @memberof CubeComponent
+   */
   private startRenderingLoop(): void {
+    // renderer
+    // use canvas element in template
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
+    let component: CubeComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    })();
   }
 }
