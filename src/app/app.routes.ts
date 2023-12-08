9062d59b4f3a51492b7a7d5f78ce535b366edd70
peggy-chien <peggychou0610@gmail.com>
2023-12-08 22:07:50 +0800

    fix(cube): fix route bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'cube', loadChildren: () => import('./components/cube/cube.component').then(mod => mod.CubeComponent) }
+  { path: 'cube', loadComponent: () => import('./components/cube/cube.component').then(mod => mod.CubeComponent) }
 ];
